4b18ce55daaa5e8c8ec5fa1477959e1fa280e02d
snowlight-aemt <it.s.aemt@gmail.com>
2024-02-15 21:40:17 +0900

    FEAT: Article 생성 테스크 코드 수정

--- src/main/kotlin/me/snowlight/springkotlincoroutine/SpringKotlinCoroutineApplication.kt
+++ src/main/kotlin/me/snowlight/springkotlincoroutine/SpringKotlinCoroutineApplication.kt
@@ -2,8 +2,10 @@ package me.snowlight.springkotlincoroutine
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 
 @SpringBootApplication
+@EnableR2dbcAuditing
 class SpringKotlinCoroutineApplication
 
 fun main(args: Array<String>) {
--- src/test/kotlin/me/snowlight/springkotlincoroutine/service/ArticleServiceTest.kt
+++ src/test/kotlin/me/snowlight/springkotlincoroutine/service/ArticleServiceTest.kt
@@ -2,6 +2,8 @@ package me.snowlight.springkotlincoroutine.service
 
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNot
+import io.kotest.matchers.shouldNotBe
 import me.snowlight.springkotlincoroutine.model.ArticleRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,9 +15,16 @@ class ArticleServiceTest(
 ) : StringSpec({
     "create and get" {
         val prev = repository.count()
-        service.create(ReqCreate(title = "title 1", body = "body 1", authorId = 100))
+        val created = service.create(ReqCreate(title = "title 1", body = "body 1", authorId = 100))
         val curr = repository.count()
 
+        val get = service.get(created.id)
+
         curr shouldBe prev + 1
+        get.id shouldBe created.id
+        get.title shouldBe created.title
+        get.authorId shouldBe created.authorId
+        get.createdAt shouldNotBe null
+        get.updatedAt shouldNotBe null
     }
 })
